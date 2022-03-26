68b4bf7074ab34e4197a61c1ebc92453504e4150
Pravin Bhat <bhatpravin@gmail.com>
2022-03-26 17:41:23 -0400

    implemented simple circuit-breaker

--- src/main/java/com/bhatman/poc/astra/flight/Flight.java
+++ src/main/java/com/bhatman/poc/astra/flight/Flight.java
@@ -1,8 +1,10 @@
 package com.bhatman.poc.astra.flight;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Flight {
     private Long flightId;
     private String flightName;
--- src/main/java/com/bhatman/poc/astra/flight/FlightController.java
+++ src/main/java/com/bhatman/poc/astra/flight/FlightController.java
@@ -1,5 +1,6 @@
 package com.bhatman.poc.astra.flight;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -8,36 +9,49 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
+@RequestMapping("/flights")
 public class FlightController {
 
+    private static final String Flight_CircuitBreaker = "FlightController";
     Map<Long, Flight> flights = new HashMap<>();
 
-    @GetMapping("/flights")
-    List<Flight> all() {
+    @GetMapping
+    @CircuitBreaker(name = Flight_CircuitBreaker, fallbackMethod = "sendAstraRegionErrorMsg")
+    public List<Flight> all() throws Exception {
+        if (Math.random() < 0.6) {
+            throw  new Exception("Unable to get Flight data!");
+        }
         return new ArrayList<>(flights.values());
     }
 
-    @PostMapping("/flights")
-    Flight newFlight(@RequestBody Flight newFlight) {
+    public List<Flight>  sendAstraRegionErrorMsg(Exception e) {
+        List<Flight> ret = new ArrayList<>();
+        ret.add(new Flight(0l, "Fallback: " + e.getMessage()));
+
+        return ret;
+    }
+
+    @PostMapping
+    public Flight newFlight(@RequestBody Flight newFlight) {
         flights.put(newFlight.getFlightId(), newFlight);
 
         return newFlight;
     }
 
-    @GetMapping("/flights/{id}")
-    Flight one(@PathVariable Long id) {
+    @GetMapping("/{id}")
+    public Flight one(@PathVariable Long id) {
         return flights.get(id);
     }
 
-    @PutMapping("/flights/{id}")
-    Flight replaceFlight(@RequestBody Flight newFlight, @PathVariable Long id) {
+    @PutMapping("/{id}")
+    public Flight replaceFlight(@RequestBody Flight newFlight, @PathVariable Long id) {
         flights.put(id, newFlight);
 
         return newFlight;
     }
 
-    @DeleteMapping("/flights/{id}")
-    void deleteFlight(@PathVariable Long id) {
+    @DeleteMapping("/{id}")
+    public void deleteFlight(@PathVariable Long id) {
         flights.remove(id);
     }
 
