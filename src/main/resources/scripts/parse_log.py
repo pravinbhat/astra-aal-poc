faf25780a70d040995035a14164ad9edd8e6cafa
Pravin Bhat <bhatpravin@gmail.com>
2024-01-18 11:29:58 -0500

    Updated with improved RegEx, query handling & queue req filtering

--- src/main/resources/scripts/parse_log.py
+++ src/main/resources/scripts/parse_log.py
@@ -9,18 +9,19 @@ if len(sys.argv) < 2:
 else:
     log_file_path = sys.argv[1]
 
+queue_time_over_millis = 5
 log_data = defaultdict(dict) # {request_id:{event_type:timestamp, duration:duration}}
 
 # Regular expression pattern to extract relevant information
-pattern = re.compile(r'(\d+:\d+:\d+\.\d+)(.*)com\.datastax\.oss\.driver\.internal\.core(.*)\[bhatman\|(\d+)[|\]](.*)')
-duration_pattern = re.compile(r'(.*)\((\d+) ms\)(.*)')
-duration_pattern_second = re.compile(r'(.*)\((\d+) s\)(.*)')
+pattern = re.compile(r'(\d+:\d+:\d+\.\d+)\s*(.*?)\s*com\.datastax\.oss\.driver\.internal\.core.(.*?)\s*:\s*\[.*?\|(\d+).*?](.*)')
+duration_pattern = re.compile(r'\[(.*?)\].*?\((.*?) ms\).*?(select .*?)\[', re.IGNORECASE)
+duration_pattern_second = re.compile(r'\[(.*?)\].*?\((.*?) s\).*?(select .*?)\[', re.IGNORECASE)
+
 with open(log_file_path, 'r') as file:
     for line in file:
         match = pattern.match(line)
         if match:
             timestamp, _, logger, request_id, remained = match.groups()
-            # print("timestamp, _, logger, request_id, remained:", timestamp, _, logger, request_id)
 
             if "Creating new handler for request" in remained:
                 event_type = "start"
@@ -29,10 +30,12 @@ with open(log_file_path, 'r') as file:
             elif "Got result" in remained:
                 event_type = "got"
             elif "RequestLogger" in logger:
-                if "SELECT * FROM airport WHERE airport=? LIMIT 1 [airport='NY-London']" not in remained:
-                    # not interested, ignore
+                if "SELECT " not in remained.upper():
                     if request_id in log_data:
                         del log_data[request_id]
+                        print(f"Deleted {request_id} for RequestLogger with log: {remained}")
+                    else:
+                        print(f"Ignoring {request_id} for RequestLogger with log: {remained}")
                     continue
                 duration_match = duration_pattern.match(remained)
                 if not duration_match:
@@ -41,11 +44,13 @@ with open(log_file_path, 'r') as file:
                         print(f"Error parsing duration: {remained}")
                         log_data[request_id]["duration"] = 'NA'
                     else:
-                        _, duration, _ = duration_match.groups()
+                        _, duration, query = duration_match.groups()
                         log_data[request_id]["duration"] = duration * 1000
+                        log_data[request_id]["query"] = query
                 else:
-                    _, duration, _ = duration_match.groups()
+                    _, duration, query = duration_match.groups()
                     log_data[request_id]["duration"] = duration
+                    log_data[request_id]["query"] = query
             else:
                 # Ignore everything else
                 continue
@@ -57,14 +62,17 @@ def str_to_milliseconds(timestamp):
     return (dt.second + dt.microsecond/1000000)*1000
 
 # Print the result
+print("requests_id,start,sent,got,duration,queue,query")
 avg_time_queued = 0
+queued_count = 0
 for request_id in log_data.keys():
     if 'sent' in log_data[request_id] and 'start' in log_data[request_id]:
         time_queued = str_to_milliseconds(log_data[request_id]['sent']) - str_to_milliseconds(log_data[request_id]['start'])
-        avg_time_queued += time_queued
-        if time_queued > 3:
-            print(f"{request_id},{log_data[request_id]['start']},{log_data[request_id]['sent']},{log_data[request_id]['got']},{log_data[request_id]['duration']},{time_queued}")
+        if time_queued > queue_time_over_millis:
+            queued_count += 1
+            print(f"{request_id},{log_data[request_id]['start']},{log_data[request_id]['sent']},{log_data[request_id]['got']},{log_data[request_id]['duration']},{time_queued},{log_data[request_id]['query']}")
+            avg_time_queued += time_queued
 
 if avg_time_queued > 0:
-    avg_time_queued /= len(log_data)
-print(f"Requests analyzed: {len(log_data)}, Average time queued: {avg_time_queued} ms")
+    avg_time_queued /= queued_count
+print(f"Requests analyzed: {len(log_data)}, Total Queued requests (over {queue_time_over_millis} ms): {queued_count}, Average time queued: {avg_time_queued} ms")
